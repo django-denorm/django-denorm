b0b35c8d4c11268f629cdd9af540c5dafaf72cbf
Christian Schilling <christian.schilling@rub.de>
2008-11-16 16:42:54 +0100

    management command works with "self" dependency

--- fields.py
+++ fields.py
@@ -12,19 +12,24 @@ class Denorm:
         self.depend = depend
         self.func = func
 
-        def saverelay(*args,**kwargs):
+        def saverelay(sender,*args,**kwargs):
             kwargs['deleted'] = False
-            func(*args,**kwargs)
+            if sender in saverelay.denorm.depend:
+                func(sender,*args,**kwargs)
         self.saverelay = saverelay
+        self.saverelay.denorm = self
+
         def deleterelay(*args,**kwargs):
             kwargs['deleted'] = True
+            if sender in deleterelay.denorm.depend:
+                func(sender,*args,**kwargs)
             func(*args,**kwargs)
         self.deleterelay = deleterelay
+        self.deleterelay.denorm = self
 
-        for sender in self.depend:
-            models.signals.pre_save.connect(pre_handler,sender)
-            models.signals.post_save.connect(self.saverelay,sender)
-            models.signals.post_delete.connect(self.deleterelay,sender)
+        models.signals.pre_save.connect(pre_handler)
+        models.signals.post_save.connect(self.saverelay)
+        models.signals.post_delete.connect(self.deleterelay)
 
     def rebuild(self):
         for model in self.depend:
@@ -37,9 +42,23 @@ def rebuildall():
         denorm.rebuild()
     
 from django.db import models
-def denormalized(DBField,depend):
+def denormalized(DBField,*args,**kwargs):
+    try:
+        depend = kwargs['depend']
+        del kwargs['depend']
+    except:
+        depend = ['self']
+
+    class DenormDBField(DBField):
+        def contribute_to_class(self,cls,*args,**kwargs):
+            global alldenorms
+            if "self" in alldenorms[-1].depend:
+                alldenorms[-1].depend += [cls]
+                alldenorms[-1].depend.remove("self")
+            DBField.contribute_to_class(self,cls,*args,**kwargs)
+
     def deco(func):
         global alldenorms
         alldenorms += [Denorm(depend,func)]
-        return DBField
+        return DenormDBField(*args,**kwargs)
     return deco
