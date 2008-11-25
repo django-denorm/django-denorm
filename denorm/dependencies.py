721ec5df762c1ff615d8635b6aad2d3f87b3cf02
Christian Schilling <christian.schilling@rub.de>
2008-11-25 22:11:13 +0100

    added relationtype auto-detection

--- denorm/dependencies.py
+++ denorm/dependencies.py
@@ -7,34 +7,35 @@ class DenormDependency:
     def setup(*args,**kwargs):
         pass
 
-class ForwardForeignKey(DenormDependency):
+class OnRelated(DenormDependency):
     def __init__(self,model,foreign_key=None):
         self.other_model = model
         self.foreign_key = foreign_key
 
-    def resolve(self,instance):
-        if isinstance(instance,self.other_model):
-            return self.this_model.objects.filter(**{self.foreign_key:instance.id})
-        else:
-            return self.this_model.objects.none()
-
-    def setup(self,this_model, **kwargs):
-        self.this_model = this_model
-        self.foreign_key = find_fk(this_model,self.other_model,self.foreign_key)
-
-class BackwardForeignKey(DenormDependency):
-    def __init__(self,model,foreign_key=None):
-        self.other_model = model
-        self.foreign_key = foreign_key
-
-    def resolve(self,instance):
+    def resolve_backward(self,instance):
         if isinstance(instance,self.other_model):
             id = getattr(instance,self.foreign_key).id
             return self.this_model.objects.filter(id__exact=id)
         else:
             return self.this_model.objects.none()
 
+    def resolve_forward(self,instance):
+        if isinstance(instance,self.other_model):
+            return self.this_model.objects.filter(**{self.foreign_key:instance.id})
+        else:
+            return self.this_model.objects.none()
+
+    def resolve(self,instance):
+        if self.type == 'forward':
+            return self.resolve_forward(instance)
+        elif self.type == 'backward':
+            return self.resolve_backward(instance)
+
     def setup(self,this_model, **kwargs):
         self.this_model = this_model
-        self.foreign_key = find_fk(self.other_model,this_model,self.foreign_key)
-
+        try:
+            self.foreign_key = find_fk(this_model,self.other_model,self.foreign_key)
+            self.type = 'forward'
+        except ValueError:
+            self.foreign_key = find_fk(self.other_model,this_model,self.foreign_key)
+            self.type = 'backward'
--- example/gallery/models.py
+++ example/gallery/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from denorm.fields import denormalized
-from denorm.dependencies import BackwardForeignKey,ForwardForeignKey
+from denorm.dependencies import OnRelated
 
 
 class Picture(models.Model):
@@ -10,7 +10,7 @@ class Picture(models.Model):
     image = models.ImageField(upload_to='photos')
     gallery = models.ForeignKey('Gallery')
 
-    @denormalized(models.CharField,max_length=100,depend=ForwardForeignKey(User))
+    @denormalized(models.CharField,max_length=100,depend=OnRelated(User))
     def owner_username(self):
         return self.owner.username
 
@@ -21,11 +21,11 @@ class Picture(models.Model):
 class Gallery(models.Model):
     name = models.CharField(max_length=100)
 
-    @denormalized(models.TextField,blank=True,depend=BackwardForeignKey(Picture))
+    @denormalized(models.TextField,blank=True,depend=OnRelated(Picture))
     def users(self):
         return ', '.join(p.owner_username for p in self.picture_set.all())
 
-    @denormalized(models.PositiveIntegerField,default=0,depend=BackwardForeignKey(Picture))
+    @denormalized(models.PositiveIntegerField,default=0,depend=OnRelated(Picture))
     def picture_count(self):
         return self.picture_set.count()
 
@@ -36,7 +36,7 @@ class Comment(models.Model):
     text = models.TextField()
     picture = models.ForeignKey(Picture)
 
-    @denormalized(models.CharField,max_length=100,depend=ForwardForeignKey(Picture))
+    @denormalized(models.CharField,max_length=100,depend=OnRelated(Picture))
     def picture_name(self):
         return self.picture.name
 
