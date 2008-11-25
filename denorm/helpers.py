ea61644323097815198978c27bdff6c2c20059be
Christian Schilling <christian.schilling@rub.de>
2008-11-25 22:32:17 +0100

    improved FK auto-detect

--- denorm/helpers.py
+++ denorm/helpers.py
@@ -1,23 +1,27 @@
 from django.db import models
 
 def find_fk(from_model, to_model, foreign_key=None):
-        if foreign_key:
-            if isinstance(foreign_key, (str, unicode)):
-                return foreign_key
-            else:
-                return foreign_key.attname
-        else:
-            fkeys = filter(lambda x: isinstance(x, models.ForeignKey)
-                                     and x.rel.to == to_model,
-                           from_model._meta.fields)
-            if not fkeys:
-                raise ValueError("%s has no ForeignKeys to %s; cannot auto-resolve."
-                                 % (from_model, to_model))
-            if len(fkeys) > 1:
-                raise ValueError("%s has more than one ForeignKey to %s;"
-                                 " please specify foreign_key= on the denorm dependency."
-                                 % (from_model, to_model))
-        if fkeys[0].attname.endswith("_id"):
-            return fkeys[0].attname[:-3]
-        else:
-            return fkeys[0].attname
+    if foreign_key:
+        if not isinstance(foreign_key, (str, unicode)):
+            foreign_key = foreign_key.attname
+        fkeys = filter(lambda x: isinstance(x, models.ForeignKey)
+                                 and x.rel.to == to_model
+                                 and (x.attname == foreign_key 
+                                   or x.attname == foreign_key+'_id'),
+                       from_model._meta.fields)
+    else:
+        fkeys = filter(lambda x: isinstance(x, models.ForeignKey)
+                                 and x.rel.to == to_model,
+                       from_model._meta.fields)
+
+    if not fkeys:
+        raise ValueError("%s has no ForeignKeys to %s; cannot auto-resolve."
+                         % (from_model, to_model))
+    if len(fkeys) > 1:
+        raise ValueError("%s has more than one ForeignKey to %s;"
+                         " please specify foreign_key= on the denorm dependency."
+                         % (from_model, to_model))
+    if fkeys[0].attname.endswith("_id"):
+        return fkeys[0].attname[:-3]
+    else:
+        return fkeys[0].attname
