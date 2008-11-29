4b0b85a54208625c7a2753d8ba9b96818f1411d0
Andrew Godwin <andrew@algae.(none)>
2008-11-29 23:06:56 +0000

    Use __all__ to make it not overwrite .models randomly.

--- denorm/__init__.py
+++ denorm/__init__.py
@@ -1,3 +1,5 @@
 
 from denorm.fields import denormalized
-from denorm.dependencies import depend_on_related,depend_on_q
+from denorm.dependencies import depend_on_related, depend_on_q
+
+__all__ = ["denormalized", "depend_on_related", "depend_on_q"]
