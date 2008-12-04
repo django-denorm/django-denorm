2b6d5cdef0229e56ebb9226a0e1dd273f17452ad
Christian Schilling <christian.schilling@rub.de>
2008-12-04 18:15:20 +0100

    fix dependency for CountField shortcut

--- denorm/shortcuts.py
+++ denorm/shortcuts.py
@@ -5,8 +5,8 @@ from denorm.dependencies import DependOnRelated
 def CountField(model,manager,fk_name=None):
 
     countfield = denormalized(models.IntegerField)(lambda i:getattr(i,manager).count())
-    dependency = DependOnRelated(model)
-    countfield.denorm.depend = [dependency]
+    dependency = DependOnRelated(model,fk_name)
+    countfield.denorm.func.depend = [dependency]
 
     return countfield
 
