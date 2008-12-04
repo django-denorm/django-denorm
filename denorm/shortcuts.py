f3de36017178464c14c353a48aad51a8925c2afb
Christian Schilling <christian.schilling@rub.de>
2008-12-04 20:02:40 +0100

    use unsigned for counts

--- denorm/shortcuts.py
+++ denorm/shortcuts.py
@@ -4,7 +4,7 @@ from denorm.dependencies import DependOnRelated
 
 def CountField(model,manager,fk_name=None):
 
-    countfield = denormalized(models.IntegerField)(lambda i:getattr(i,manager).count())
+    countfield = denormalized(models.PositiveIntegerField)(lambda i:getattr(i,manager).count())
     dependency = DependOnRelated(model,fk_name)
     countfield.denorm.func.depend = [dependency]
 
