db5ffaede1f6b82ac6a7fbb7854fdbeeb550aab3
Christian Schilling <christian@initcrash.net>
2009-03-27 15:36:57 +0100

    use only a single management command with subcommands

--- denorm/management/commands/denormalize.py
+++ denorm/management/commands/denormalize.py
@@ -3,4 +3,19 @@ from denorm import fields
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        fields.rebuildall()
+        if args:
+            if args[0] == "rebuild":
+                fields.rebuildall()
+                return
+            if args[0] == "init":
+                fields.install_triggers()
+                return
+            if args[0] == "flush":
+                fields.flush()
+                return
+
+        print "unknown subcommand"
+        print "subcommands are:"
+        print "    rebuild"
+        print "    init"
+        print "    flush"
--- denorm/management/commands/init_denorm.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.base import BaseCommand
-from denorm import fields
-
-class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        fields.install_triggers()
