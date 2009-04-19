7dcc8b0946d08f0ab491311b15454d8bcd6e51e9
Christian Schilling <christian@initcrash.net>
2009-04-19 18:30:54 +0200

    fix problem when using south's orm freezer.
    the problem was that the == operator returns False when
    comparing a real model with the corresponding frozen model.
    to solve this we now just compare their _meta.db_table.

--- denorm/helpers.py
+++ denorm/helpers.py
@@ -5,7 +5,7 @@ def find_fk(from_model, to_model, foreign_key=None):
         if not isinstance(foreign_key, (str, unicode)):
             foreign_key = foreign_key.attname
         fkeys = filter(lambda x: isinstance(x, models.ForeignKey)
-                                 and x.rel.to == to_model
+                                 and x.rel.to._meta.db_table == to_model._meta.db_table
                                  and x.attname in [foreign_key,foreign_key+'_id'],
                        from_model._meta.fields)
     else:
