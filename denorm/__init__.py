b0f41d6dd694ee4c3e0762c3eb52dbf70af7269e
Christian Schilling <christian@christian-nb.(none)>
2009-05-15 17:41:01 +0200

    really removed import of nonexistent module

--- denorm/__init__.py
+++ denorm/__init__.py
@@ -3,6 +3,4 @@ from denorm.fields import denormalized,flush
 from denorm.dependencies import depend_on_related
 from denorm.shortcuts import CountField
 
-import denorm.monkeypatches
-
 __all__ = ["denormalized", "depend_on_related","flush", "CountField"]
