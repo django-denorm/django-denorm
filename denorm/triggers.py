bfa8dc7ba39cced709107c3e78ef0a670d503a72
Christian Schilling <christian@christian-nb.(none)>
2009-05-15 21:57:11 +0200

    make sure all old triggers get dropped when installing triggers

--- denorm/triggers.py
+++ denorm/triggers.py
@@ -90,9 +90,11 @@ class TriggerSet:
     def install(self):
         from django.db import connection
         cursor = connection.cursor()
+
+        cursor.execute("SHOW TRIGGERS;")
+        for result in cursor.fetchall():
+            if result[0].startswith("denorm_"):
+                cursor.execute("""DROP TRIGGER %s;""" % result[0])
+
         for name,trigger in self.triggers.iteritems():
-            try:
-                cursor.execute("""DROP TRIGGER %s;""" % name)
-            except:
-                pass
             cursor.execute(trigger.sql())
