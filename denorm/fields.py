ffdcbc0fa8639a6024afd1624910ca3b2ab612b6
Christian Schilling <christian@christian-nb.(none)>
2009-05-15 17:15:36 +0200

    ramoved import of nonexistent module

--- denorm/fields.py
+++ denorm/fields.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from denorm import monkeypatches
 
 # remember all denormalizations.
 # this is used to rebuild all denormalized values in the whole DB
