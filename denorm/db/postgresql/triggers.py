f527b5fcfe6cdc35db571875d7ae03244ae9cdc1
Christian Schilling <christian@initcrash.net>
2009-05-19 16:34:09 +0200

    add support for postgresql

---
+++ denorm/db/postgresql/triggers.py
@@ -0,0 +1,70 @@
+from denorm.db import base
+
+class TriggerNestedSelect(base.TriggerNestedSelect):
+
+    def sql(self):
+        columns = self.columns
+        table = self.model._meta.db_table
+        where = ",".join(["%s = %s"%(k,v) for k,v in self.kwargs.iteritems()])
+        return """ SELECT DISTINCT %(columns)s FROM %(table)s WHERE %(where)s """ % locals()
+
+class TriggerActionInsert(base.TriggerActionInsert):
+
+    def sql(self):
+        table = self.model._meta.db_table
+        columns = "("+",".join(self.columns)+")"
+        if isinstance(self.values,TriggerNestedSelect):
+            values = "("+self.values.sql()+")"
+        else:
+            values = "VALUES("+",".join(self.values)+")"
+
+        return (
+             """ BEGIN\n"""
+            +"""     INSERT INTO %(table)s %(columns)s %(values)s;\n"""
+            +"""    EXCEPTION WHEN unique_violation THEN\n     -- do nothing\n """
+            +"""   END"""
+            ) % locals()
+
+class Trigger(base.Trigger):
+
+    def sql(self):
+        name = self.name()
+        actions = (";\n   ").join(set([a.sql() for a in self.actions if a.sql()])) + ";"
+        table = self.model._meta.db_table
+        time = self.time.upper()
+        event = self.event.upper()
+
+        if event == "UPDATE":
+            fieldnames = [x.attname for x in self.model._meta.fields]
+            cond = "(%s)"%"OR".join(["(OLD.%s!=NEW.%s)"%(f,f) for f in fieldnames])
+        else:
+            cond = 'TRUE'
+
+        return (
+             """ CREATE OR REPLACE FUNCTION func_%(name)s()\n"""
+            +"""  RETURNS TRIGGER AS $$\n"""
+            +"""  BEGIN\n"""
+            +"""   IF %(cond)s THEN\n"""
+            +"""    %(actions)s\n"""
+            +"""   END IF;\n"""
+            +"""  RETURN NULL; END;\n"""
+            +"""  $$ LANGUAGE plpgsql;\n"""
+            +"""  CREATE TRIGGER %(name)s\n"""
+            +"""  %(time)s %(event)s ON %(table)s\n"""
+            +"""  FOR EACH ROW EXECUTE PROCEDURE func_%(name)s();\n"""
+            ) % locals()
+
+class TriggerSet(base.TriggerSet):
+
+    def install(self):
+        from django.db import connection
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM pg_trigger;")
+        for result in cursor.fetchall():
+            if result[1].startswith("denorm_"):
+                x,table = result[1].rsplit("_on_",)
+                cursor.execute("""DROP TRIGGER %s ON %s;""" % (result[1],table))
+
+        for name,trigger in self.triggers.iteritems():
+            cursor.execute(trigger.sql())
