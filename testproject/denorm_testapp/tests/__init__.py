b7b4b4f01913028ffc1fb4c69ac1cac5141bf32b
Christian Schilling <christian@initcrash.net>
2009-05-21 14:44:50 +0200

    moved tests into seperate project

--- testproject/denorm_testapp/tests/__init__.py
+++ testproject/denorm_testapp/tests/__init__.py
@@ -23,44 +23,14 @@ class TestDenormalisation(unittest.TestCase):
     """
 
     def setUp(self):
-        """Swaps out various Django calls for fake ones for our own nefarious purposes."""
-
-        def new_get_apps():
-            return [denorm_testapp.models]
-
-        from django.db import models
-        from django.conf import settings
-        models.get_apps_old, models.get_apps = models.get_apps, new_get_apps
-        settings.INSTALLED_APPS, settings.OLD_INSTALLED_APPS = (
-            ["denorm_testapp"],
-            settings.INSTALLED_APPS,
-        )
-        self.redo_app_cache()
-        management.call_command('syncdb')
         install_triggers()
 
 
     def tearDown(self):
-        """Undoes what monkeypatch did."""
-
-        from django.db import models
-        from django.conf import settings
-        models.get_apps = models.get_apps_old
-        settings.INSTALLED_APPS = settings.OLD_INSTALLED_APPS
-        self.redo_app_cache()
-
-        # Also delete all model instances
+        # delete all model instances
         Attachment.objects.all().delete()
         Post.objects.all().delete()
         Forum.objects.all().delete()
-
-
-    def redo_app_cache(self):
-        from django.db.models.loading import AppCache
-        a = AppCache()
-        a.loaded = False
-        a._populate()
-
 
     def test_depends_related(self):
         """
