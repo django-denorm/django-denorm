bd8304035fd937e00227a501daaac35110fa29a7
Christian Schilling <christian@initcrash.net>
2009-05-23 16:49:23 +0200

    code cleanup&comments

--- denorm/dependencies.py
+++ denorm/dependencies.py
@@ -5,39 +5,55 @@ from denorm.models import DirtyInstance
 from django.contrib.contenttypes.models import ContentType
 from denorm.db import triggers
 
-def make_depend_decorator(Resolver):
-    def decorator(*args,**kwargs):
-        def deco(func):
-            if not hasattr(func,'depend'):
-                func.depend = []
-            func.depend += [Resolver(*args,**kwargs)]
-            return func
-        return deco
-    return decorator
 
-class DenormDependency:
-    def resolve(*args,**kwargs):
-        return None
-    def setup(*args,**kwargs):
-        pass
+class DenormDependency(object):
+
+    """
+    Base class for real dependency classes.
+    """
+
+    def get_triggers(self):
+        """
+        Must return a list of ``denorm.triggers.Trigger`` instances
+        """
+        return []
+
+    def setup(self, this_model):
+        """
+        Remembers the model this dependency was declared in.
+        """
+        self.this_model = this_model
 
 class DependOnRelated(DenormDependency):
+
+    """
+    A DenormDependency that handles callbacks depending on fields
+    in other models that are related to the dependent model.
+
+    Two models are considered related if there is a ForeignKey or ManyToManyField
+    on either of them pointing to the other one.
+    """
+
     def __init__(self,model,foreign_key=None,type=None):
         self.other_model = model
-        self.foreign_key = foreign_key
+        self.fk_name = foreign_key
         self.type = type
 
     def get_triggers(self):
         content_type = str(ContentType.objects.get_for_model(self.this_model).id)
 
         if self.type == "forward":
+            # With forward relations many instances of ``this_model``
+            # may be related to one instance of ``other_model``
+            # so we need to do a nested select query in the trigger
+            # to find them all.
             action_new = triggers.TriggerActionInsert(
                 model = DirtyInstance,
                 columns = ("content_type_id","object_id"),
                 values = triggers.TriggerNestedSelect(
                     self.this_model._meta.db_table,
                     (content_type,"id"),
-                    **{self.foreign_key:"NEW.id"}
+                    **{self.field.attname:"NEW.id"}
                 )
             )
             action_old = triggers.TriggerActionInsert(
@@ -46,7 +62,7 @@ class DependOnRelated(DenormDependency):
                 values = triggers.TriggerNestedSelect(
                     self.this_model._meta.db_table,
                     (content_type,"id"),
-                    **{self.foreign_key:"OLD.id"}
+                    **{self.field.attname:"OLD.id"}
                 )
             )
             return [
@@ -56,12 +72,17 @@ class DependOnRelated(DenormDependency):
             ]
 
         if self.type == "backward":
+            # With backward relations a change in ``other_model`` can affect
+            # only one or two instances of ``this_model``.
+            # If the ``other_model`` instance changes the value its ForeignKey
+            # pointing to ``this_model`` both the old and the new related instance
+            # are affected, otherwise only the one it is pointing to is affected.
             action_new = triggers.TriggerActionInsert(
                 model = DirtyInstance,
                 columns = ("content_type_id","object_id"),
                 values = (
                     content_type,
-                    "NEW.%s" % self.foreign_key,
+                    "NEW.%s" % self.field.attname,
                 )
             )
             action_old = triggers.TriggerActionInsert(
@@ -69,7 +90,7 @@ class DependOnRelated(DenormDependency):
                 columns = ("content_type_id","object_id"),
                 values = (
                     content_type,
-                    "OLD.%s" % self.foreign_key,
+                    "OLD.%s" % self.field.attname,
                 )
             )
             return [
@@ -79,6 +100,8 @@ class DependOnRelated(DenormDependency):
             ]
 
         if "m2m" in self.type:
+            # The two directions of M2M relations only differ in the column
+            # names used in the intermediate table.
             if "forward" in self.type:
                 column_name = self.field.m2m_column_name()
                 reverse_column_name = self.field.m2m_reverse_name()
@@ -86,6 +109,9 @@ class DependOnRelated(DenormDependency):
                 column_name = self.field.m2m_reverse_name()
                 reverse_column_name = self.field.m2m_column_name()
 
+            # The first part of a M2M dependency is exactly like a backward
+            # ForeignKey dependency. ``this_model`` is backward FK related
+            # to the intermediate table.
             action_m2m_new = triggers.TriggerActionInsert(
                 model = DirtyInstance,
                 columns = ("content_type_id","object_id"),
@@ -102,6 +128,12 @@ class DependOnRelated(DenormDependency):
                     "OLD.%s" % column_name,
                 )
             )
+
+            # Additionally to the dependency on the intermediate table
+            # ``this_model`` is dependant on updates to the ``other_model``-
+            # There is no need to track insert or delete events here,
+            # because a relation can only be created or deleted by
+            # by modifying the intermediate table.
             action_new = triggers.TriggerActionInsert(
                 model = DirtyInstance,
                 columns = ("content_type_id","object_id"),
@@ -122,28 +154,36 @@ class DependOnRelated(DenormDependency):
         return []
 
 
-    def setup(self,this_model, **kwargs):
-        self.this_model = this_model
+    def setup(self, this_model):
+        super(DependOnRelated,self).setup(this_model)
 
         # FIXME: this should not be necessary
         if self.other_model == related.RECURSIVE_RELATIONSHIP_CONSTANT:
             self.other_model = self.this_model
 
         if isinstance(self.other_model,(str,unicode)):
+            # if ``other_model`` is a string, it certainly is a lazy relation.
             related.add_lazy_relation(self.this_model, None, self.other_model, self.resolved_model)
         else:
+            # otherwise it can be resolved directly
             self.resolved_model(None,self.other_model,None)
 
     def resolved_model(self, data, model, cls):
+        """
+        Does all the initialization that had to wait until we knew which
+        model we depend on.
+        """
         self.other_model = model
 
-        candidates =  [('forward',fk) for fk in find_fks(self.this_model,self.other_model,self.foreign_key)]
-        candidates += [('backward',fk) for fk in find_fks(self.other_model,self.this_model,self.foreign_key)]
-        candidates += [('forward_m2m',fk) for fk in find_m2ms(self.this_model,self.other_model,self.foreign_key)]
-        candidates += [('backward_m2m',fk) for fk in find_m2ms(self.other_model,self.this_model,self.foreign_key)]
+        # Create a list of all ForeignKeys and ManyToManyFields between both related models, in both directions
+        candidates  = [('forward',fk) for fk in find_fks(self.this_model,self.other_model,self.fk_name)]
+        candidates += [('backward',fk) for fk in find_fks(self.other_model,self.this_model,self.fk_name)]
+        candidates += [('forward_m2m',fk) for fk in find_m2ms(self.this_model,self.other_model,self.fk_name)]
+        candidates += [('backward_m2m',fk) for fk in find_m2ms(self.other_model,self.this_model,self.fk_name)]
 
-        if self.type:
-            candidates = [x for x in candidates if self.type == x[0]]
+        # If a relation type was given (forward,backward,forward_m2m or backward_m2m),
+        # filter out all relations that do not match this type.
+        candidates = [x for x in candidates if not self.type or self.type == x[0]]
 
         if len(candidates) > 1:
             raise ValueError("%s has more than one ForeignKey or ManyToManyField to %s (or reverse); cannot auto-resolve."
@@ -152,11 +192,24 @@ class DependOnRelated(DenormDependency):
             raise ValueError("%s has no ForeignKeys or ManyToManyFields to %s (or reverse); cannot auto-resolve."
                              % (self.this_model, self.other_model))
 
-        winner = candidates[0]
-        self.type = winner[0]
-        self.foreign_key = winner[1].attname
-        self.field = winner[1]
+        # Now the candidates list contains exactly one item, thats our winner.
+        self.type, self.field = candidates[0]
 
+
+def make_depend_decorator(Class):
+    """
+    Create a decorator that attaches an instance of the given class
+    to the decorated function, passing all remaining arguments to the classes
+    __init__.
+    """
+    def decorator(*args,**kwargs):
+        def deco(func):
+            if not hasattr(func,'depend'):
+                func.depend = []
+            func.depend += [Class(*args,**kwargs)]
+            return func
+        return deco
+    return decorator
 
 depend_on_related = make_depend_decorator(DependOnRelated)
 
--- denorm/fields.py
+++ denorm/fields.py
@@ -6,17 +6,13 @@ from django.db import models
 alldenorms = []
 
 class Denorm:
+
     """
     Handles the denormalization of one field.
-    Everytime some model instances are updated a queryset is build, that will contain
-    all instances of the model containing the field that may need to get
-    updated.
-    This is done by passing a QuerySet containing all instances beeing
-    updated into the resolve() method of all DenormDependency
-    subclass instances in 'depend', and combining the results with logical OR.
     """
 
     def __init__(self,func):
+        # ensure self.func is always a list
         if not hasattr(func,'depend'):
             func.depend = []
         self.func = func
@@ -30,11 +26,13 @@ class Denorm:
 
     def setup(self,**kwargs):
         """
-        Calls setup() on all DenormDependency resolvers
+        Adds 'self' to the global denorm list,
+        calls setup() on all DenormDependency resolvers
         and connects all needed signals.
         """
         global alldenorms
         alldenorms += [self]
+
         for dependency in self.func.depend:
             dependency.setup(self.model)
 
@@ -53,12 +51,23 @@ class Denorm:
         flush()
 
     def get_triggers(self):
+        """
+        Creates a list of all triggers needed to keep track of changes
+        to fields this denorm depends on.
+        """
         from denorm.db import triggers
         from django.contrib.contenttypes.models import ContentType
         from denorm.models import DirtyInstance
 
         content_type = str(ContentType.objects.get_for_model(self.model).id)
 
+        # Create a trigger that marks any updated or newly created
+        # instance of the model containing the denormalized field
+        # as dirty.
+        # This is only really needed if the instance was changed without
+        # using the ORM or if it was part of a bulk update.
+        # In those cases the self_save_handler won't get called by the
+        # pre_save signal, so we need to ensure flush() does this later.
         action = triggers.TriggerActionInsert(
             model = DirtyInstance,
             columns = ("content_type_id","object_id"),
@@ -69,6 +78,8 @@ class Denorm:
             triggers.Trigger(self.model,"after","insert",[action]),
         ]
 
+        # Get the triggers of all DenormDependency instances attached
+        # to our callback.
         for dependency in self.func.depend:
             trigger_list += dependency.get_triggers()
 
@@ -84,24 +95,43 @@ def rebuildall():
         denorm.update(denorm.model.objects.all())
 
 def install_triggers():
+    """
+    Installs all required triggers in the database
+    """
     from denorm.db import triggers
     global alldenorms
 
+    # Use a TriggerSet to ensure each event gets just one trigger
     triggerset = triggers.TriggerSet()
     for denorm in alldenorms:
         triggerset.append(denorm.get_triggers())
     triggerset.install()
 
 def flush():
+    """
+    Updates all model instances marked as dirty by the DirtyInstance
+    model.
+    After this method finishes the DirtyInstance table is empty and
+    all denormalized fields have consistent data.
+    """
     from denorm.models import DirtyInstance
 
-    qs = DirtyInstance.objects.all()
-    while qs:
+    # Loop until break.
+    # We may need multiple passes, because an update on one instance
+    # may cause an other instance to be marked dirty (dependency chains)
+    while True:
+        # Get all dirty markers
+        qs = DirtyInstance.objects.all()
+
+        # DirtyInstance table is empty -> all data is consistent -> we're done
+        if not qs: break
+
+        # Call save() on all dirty instances, causing the self_save_handler()
+        # getting called by the pre_save signal.
         for dirty_instance in qs:
             if dirty_instance.content_object:
                 dirty_instance.content_object.save()
             dirty_instance.delete()
-        qs = DirtyInstance.objects.all()
 
 def denormalized(DBField,*args,**kwargs):
 
@@ -142,8 +172,6 @@ def denormalized(DBField,*args,**kwargs):
                     [repr(x) for x in args],
                     kwargs,
                 )
-
-
 
     def deco(func):
         denorm = Denorm(func)
