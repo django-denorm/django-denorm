730b1fd25e9524d618f679d8cbbd1bffe252a561
James Turnbull <admin@incuna.com>
2010-06-09 12:47:35 +0100

    Added initial fix for Django 1.2 DATABASE dicts (only uses defualt for just now)

--- denorm/db/__init__.py
+++ denorm/db/__init__.py
@@ -1,9 +1,19 @@
 from django.conf import settings
 
-triggers_module_name = ['denorm.db', settings.DATABASE_ENGINE,'triggers']
+if hasattr(settings, 'DATABASE_ENGINE') and settings.DATABASE_ENGINE:
+    print "ENGINE US %s" % settings.DATABASE_ENGINE
+    backend = settings.DATABASE_ENGINE
+else:
+    # Assume Django 1.2
+    from django.db import connections, DEFAULT_DB_ALIAS
+    engine = connections[DEFAULT_DB_ALIAS].settings_dict['ENGINE']
+    backend = engine.rsplit(".", 1)[1]
+    print backend
+
+triggers_module_name = ['denorm.db', backend, 'triggers']
 try:
     triggers = __import__('.'.join(triggers_module_name),{},{},[''])
 except ImportError:
-    raise ImportError("There is no django-denorm database module for the engine '%s'. Please either choose a supported one, or remove 'denorm' from INSTALLED_APPS.\n" % settings.DATABASE_ENGINE)
+    raise ImportError("There is no django-denorm database module for the engine '%s'. Please either choose a supported one, or remove 'denorm' from INSTALLED_APPS.\n" % backend)
 
 
