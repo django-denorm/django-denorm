937b9374f91585e8fc079594381dd783ad287e80
Max Peterson <max@cochin.local>
2010-09-24 11:34:34 +0100

    Added setting for the DENORM_BACKEND and added default mapping from database ENGINE to DENORM_BACKEND

--- denorm/db/__init__.py
+++ denorm/db/__init__.py
@@ -1,12 +1,21 @@
 from django.conf import settings
 
+if hasattr(settings, 'DATABASE_DENORM_BACKEND'):
+    backend = settings.DATABASE_DENORM_BACKEND
 if hasattr(settings, 'DATABASE_ENGINE') and settings.DATABASE_ENGINE:
     backend = settings.DATABASE_ENGINE
 else:
     # Assume Django 1.2
+    DB_MAPPING = {'postgis':'postgresql',
+                  'postgresql_psycopg2':'postgresql',
+                 }
     from django.db import connections, DEFAULT_DB_ALIAS
-    engine = connections[DEFAULT_DB_ALIAS].settings_dict['ENGINE']
-    backend = engine.rsplit(".", 1)[1]
+    if 'DENORM_BACKEND' in connections[DEFAULT_DB_ALIAS].settings_dict:
+        backend = connections[DEFAULT_DB_ALIAS].settings_dict['DENORM_BACKEND']
+    else:
+        engine = connections[DEFAULT_DB_ALIAS].settings_dict['ENGINE']
+        backend = engine.rsplit(".", 1)[1]
+        backend = DB_MAPPING.get(backend, backend)
 
 triggers_module_name = ['denorm.db', backend, 'triggers']
 try:
