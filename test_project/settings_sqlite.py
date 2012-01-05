a14db52e98e6971c5e9dcc3b1116d5daae50383d
zyegfryed <zyegfryed@gmail.com>
2012-01-05 14:01:26 +0100

    Removed local testing code.

--- test_project/settings_sqlite.py
+++ test_project/settings_sqlite.py
@@ -3,7 +3,6 @@ from settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': ':memory:',
-        'NAME': 'test_project.db',
+        'NAME': ':memory:',
     }
 }
