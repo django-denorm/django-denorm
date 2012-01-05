6301bffcdc8f1105ad48b90f7dd7004aec1778d2
zyegfryed <zyegfryed@gmail.com>
2012-01-05 14:45:29 +0100

    Renamed using option to database to be more Django compliant.

--- denorm/management/commands/denorm_drop.py
+++ denorm/management/commands/denorm_drop.py
@@ -1,14 +1,19 @@
-from django.core.management.base import NoArgsCommand
 from optparse import make_option
+
+from django.core.management.base import NoArgsCommand
+from django.db import DEFAULT_DB_ALIAS
+
+from denorm import denorms
 
 
 class Command(NoArgsCommand):
     option_list = NoArgsCommand.option_list + (
-        make_option('--using', dest='using', help='Select database connection'),
+        make_option('--database', action='store', dest='database',
+            default=DEFAULT_DB_ALIAS, help='Nominates a database to execute '
+                'SQL into. Defaults to the "default" database.'),
     )
     help = "Removes all triggers created by django-denorm."
 
     def handle_noargs(self, **options):
-        from denorm import denorms
-        using = options.get('using')
+        using = options['database']
         denorms.drop_triggers(using=using)
