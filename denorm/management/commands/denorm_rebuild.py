65ca2b7b10059ed315198bc5f231aca71e02fad2
Christian Schilling <christian.schilling@rub.de>
2013-03-24 19:50:31 +0100

    fix rebuild verbosity handling

--- denorm/management/commands/denorm_rebuild.py
+++ denorm/management/commands/denorm_rebuild.py
@@ -5,5 +5,6 @@ from denorm import denorms
 class Command(BaseCommand):
     help = "Recalculates the value of every single denormalized model field in the whole project."
 
-    def handle(self, verbose=False, model_name=None, *args, **kwargs):
-        denorms.rebuildall(verbose=verbose, model_name=model_name)
+    def handle(self, model_name=None, *args, **kwargs):
+        verbosity = int((kwargs.get('verbosity',0)))
+        denorms.rebuildall(verbose=verbosity > 1, model_name=model_name)
