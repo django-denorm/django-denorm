87a82b153d93519b2051ba0163b66bdbb24f3296
Petr Dlouhý <petr.dlouhy@email.cz>
2015-10-14 15:18:12 +0200

    replace south migrations with south migrations

--- denorm/migrations/0001_initial.py
+++ denorm/migrations/0001_initial.py
@@ -1,47 +1,47 @@
-# encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-
-        # Adding model 'DirtyInstance'
-        db.create_table('denorm_dirtyinstance', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
-            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
-        ))
-        db.send_create_signal('denorm', ['DirtyInstance'])
-
-        # Adding unique constraint on 'DirtyInstance', fields ['object_id', 'content_type']
-        db.create_unique('denorm_dirtyinstance', ['object_id', 'content_type_id'])
-
-    def backwards(self, orm):
-
-        # Removing unique constraint on 'DirtyInstance', fields ['object_id', 'content_type']
-        db.delete_unique('denorm_dirtyinstance', ['object_id', 'content_type_id'])
-
-        # Deleting model 'DirtyInstance'
-        db.delete_table('denorm_dirtyinstance')
+from django.db import migrations, models
 
 
-    models = {
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'denorm.dirtyinstance': {
-            'Meta': {'unique_together': "(('object_id', 'content_type'),)", 'object_name': 'DirtyInstance'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'})
-        }
-    }
+class Migration(migrations.Migration):
 
-    complete_apps = ['denorm']
+    dependencies = [
+        ('contenttypes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='DenormModel',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.TextField()),
+                ('ham', models.TextField(editable=False)),
+                ('spam', models.TextField(editable=False)),
+            ],
+            options={
+                'abstract': False,
+                'swappable': 'DENORM_MODEL',
+            },
+        ),
+        migrations.CreateModel(
+            name='DirtyInstance',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('object_id', models.TextField(null=True, blank=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='RealDenormModel',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.TextField()),
+                ('ham', models.TextField(editable=False)),
+                ('eggs', models.TextField(editable=False)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+    ]
